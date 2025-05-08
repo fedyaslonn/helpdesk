2aec2d19fbf03c8c000b55387e3fd74ea257b57e
fed0r <wisp47344@gmail.com>
2025-05-08 19:12:18 +0300

    register test_example in admin panel

--- helpdesk/testapp/admin.py
+++ helpdesk/testapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import TestExample
 
 # Register your models here.
+
+@admin.register(TestExample)
+class TestExampleAdmin(admin.ModelAdmin):
+    list_display = ["content", "date_created"]
+    ordering = ["date_created"]
+    search_fields = ["content"]
