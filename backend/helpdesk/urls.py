3d5974bfd453345f61a4f740369d80f1a3e04c19
fed0r <wisp47344@gmail.com>
2025-06-04 00:10:58 +0300

    update urls

--- backend/helpdesk/urls.py
+++ backend/helpdesk/urls.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("helpdesk/", include("core.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("helpdesk/", include("core.urls"))
+]
