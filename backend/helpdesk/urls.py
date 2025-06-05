f72f1d804eacbdabf25c37dc6615614eb99fc7dd
fed0r <wisp47344@gmail.com>
2025-06-05 11:29:16 +0300

    add comma to urls

--- backend/helpdesk/urls.py
+++ backend/helpdesk/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("helpdesk/", include("core.urls"))
+    path("helpdesk/", include("core.urls")),
 ]
