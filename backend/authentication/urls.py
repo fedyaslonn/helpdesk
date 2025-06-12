7e3ba0fb2d4189efe886ae2acd90580e97553c9d
fed0r <wisp47344@gmail.com>
2025-06-12 15:20:03 +0300

    precommit changes

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,20 +1,24 @@
-from django.urls import path
 from authentication.views import (
+    CheckAuthView,
+    CustomTokenBlackListView,
     CustomTokenObtainPairView,
     CustomTokenRefreshView,
-    CustomTokenBlackListView,
     LogoutView,
-    CheckAuthView
 )
+from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
 urlpatterns = [
-    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('api/token/blacklist/', CustomTokenBlackListView.as_view(), name='token_blacklist'),
-    path('logout/', LogoutView.as_view(), name='logout_view'),
-    path('api/token/check_auth/', CheckAuthView.as_view(), name='check_auth'),
+    path("api/token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", CustomTokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path(
+        "api/token/blacklist/",
+        CustomTokenBlackListView.as_view(),
+        name="token_blacklist",
+    ),
+    path("logout/", LogoutView.as_view(), name="logout_view"),
+    path("api/token/check_auth/", CheckAuthView.as_view(), name="check_auth"),
 ]
--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,47 +1,42 @@
-from datetime import timezone
-from http.client import responses
-
-from authentication.serializers import CustomTokenObtainPairSerializer, CustomTokenRefreshSerializer, CustomTokenBlackListSerializer, LogoutSerializer
-from rest_framework_simplejwt.views import TokenVerifyView, TokenRefreshView, TokenBlacklistView, TokenObtainPairView
-from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework_simplejwt.token_blacklist.models import OutstandingToken, BlacklistedToken
-from rest_framework_simplejwt.authentication import JWTAuthentication
-
+from authentication.serializers import (
+    CustomTokenBlackListSerializer,
+    CustomTokenObtainPairSerializer,
+    CustomTokenRefreshSerializer,
+    LogoutSerializer,
+)
 from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
+from rest_framework.views import APIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.token_blacklist.models import (
+    BlacklistedToken,
+    OutstandingToken,
+)
+from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
+from rest_framework_simplejwt.views import (
+    TokenBlacklistView,
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 # Create your views here.
 
+
 class CustomTokenObtainPairView(TokenObtainPairView):
-    serializer_class = CustomTokenObtainPairSerializer
-    permission_classes = [AllowAny]
-
-    def post(self, request, *args, **kwargs):
-        response = super().post(request, *args, **kwargs)
-        if response.status_code == 200:
-            access_token = response.data['access']
-            refresh_token = response.data['refresh']
-
-            user_id = response.data.get('user_id')
-            email = response.data.get('email')
-
-            response.set_cookie('access_token', access_token)
-            response.set_cookie('refresh_token', refresh_token)
-            response.set_cookie('user_id', user_id)
-            response.set_cookie('email', email)
-
-        return response
+    serializer_class = CustomTokenObtainPairSerializer  # type: ignore
+    permission_classes = (AllowAny,)  # type: ignore
 
 
 class CustomTokenRefreshView(TokenRefreshView):
-    serializer_class = CustomTokenRefreshSerializer
+    serializer_class = CustomTokenRefreshSerializer  # type: ignore
 
 
 class CustomTokenBlackListView(TokenBlacklistView):
-    serializer_class = CustomTokenBlackListSerializer
+    serializer_class = CustomTokenBlackListSerializer  # type: ignore
 
 
 class LogoutView(APIView):
@@ -49,38 +44,30 @@ class LogoutView(APIView):
         serializer = LogoutSerializer(data=request.data)
 
         serializer.is_valid(raise_exception=True)
-        token = serializer.validated_data['refresh_token']
+        token = serializer.validated_data["refresh_token"]
 
         try:
             token.blacklist()
 
-            user_id = token.payload.get('user_id')
+            user_id = token.payload.get("user_id")
 
-            active_tokens = OutstandingToken.objects.filter(
-                user_id=user_id
-            )
+            active_tokens = OutstandingToken.objects.filter(user_id=user_id)
 
             for active_token in active_tokens:
                 if not BlacklistedToken.objects.filter(token=active_token).exists():
                     BlacklistedToken.objects.create(token=active_token)
 
-
             response = Response(
                 {"detail": "Successfully logged out"},
-                status=status.HTTP_205_RESET_CONTENT
+                status=status.HTTP_205_RESET_CONTENT,
             )
-            response.delete_cookie('access_token')
-            response.delete_cookie('refresh_token')
-            response.delete_cookie('user_id')
-            response.delete_cookie('email')
 
             return response
-
 
         except Exception as e:
             return Response(
                 {"detail": "An error occurred during logout"},
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
 
 
@@ -89,12 +76,12 @@ class CheckAuthView(APIView):
         if request.user.is_authenticated:
             return Response(
                 {
-                    'user': {
-                        'id': request.user.id,
-                        'email': request.user.email,
+                    "user": {
+                        "id": request.user.id,
+                        "email": request.user.email,
                     }
                 }
             )
 
         else:
-            return Response({'user': 'Unauthorized'}, status=401)
+            return Response({"user": "Unauthorized"}, status=401)
