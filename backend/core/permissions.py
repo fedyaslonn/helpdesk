e91bddceed82c679f7af0a4da64663e2a3e72fb9
fed0r <wisp47344@gmail.com>
2025-06-14 02:42:10 +0300

    impl permissions

---
+++ backend/core/permissions.py
@@ -0,0 +1,21 @@
+from rest_framework import permissions
+from django.db.models import Q
+
+from core.models import Membership, Organization
+
+
+class IsAdminOfOrganization(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.memberships.filter(
+            user=request.user, role=Membership.Role.ADMIN, is_active=True
+        ).exists()
+
+
+class IsAdminOrAssignee(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return (
+            obj.organization.memberships.filter(
+                user=request.user, role=Membership.Role.ADMIN, is_active=True
+            ).exists()
+            or obj.assignee.id == request.user.id
+        )
