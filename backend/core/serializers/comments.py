da5ada4b14404d64c3da290e0746800c1425d65a
fed0r <wisp47344@gmail.com>
2025-06-17 18:59:14 +0300

    update comments serializer

--- backend/core/serializers/comments.py
+++ backend/core/serializers/comments.py
@@ -19,6 +19,8 @@ class CreateCommentSerializer(serializers.ModelSerializer):
 
 
 class GetCommentSerializer(serializers.ModelSerializer):
+    author = GetUserSerializer(read_only=True)
+
     class Meta:
         model = Comment
         fields = "__all__"
